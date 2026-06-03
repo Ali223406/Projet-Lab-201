0ad045e8671df20453923de4c5f8c9e13f36fb3f
Alicia Mazo <alicianmazo@gmail.com>
2026-06-03 02:12:05 +0200

    le commit

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,51 +1,58 @@
 import "./Album.css";
+
 export default function Album() {
   return (
     <div className="album-page">
 
-      {/* BACKGROUND */}
       <div className="album-bg" />
       <div className="album-overlay" />
 
-      {/* CONTENT */}
       <div className="album-container">
 
-        {/* TITLE */}
-        <h1 className="album-title">STOVE</h1>
+        <h1 className="album-title">ALBUM : STOVE</h1>
 
         <div className="album-grid">
 
-          {/* LEFT IMAGE */}
-          <div className="album-cover">
-            <img src="/images/lanadis.jpeg" alt="Album Cover" />
-          </div>
+          {/* LEFT */}
+          <div className="album-left">
 
-          {/* RIGHT CONTENT */}
-          <div className="album-info">
-
-            <h2>About the Album</h2>
-
-            <p>
-              STOVE is the 10th studio album by Lana Del Rey.
-              A cinematic americana story about memory, home,
-              nostalgia and love. The project blends intimate
-              storytelling with dreamy soundscapes and emotional songwriting.
-            </p>
-
-            <h3>Tracklist</h3>
-
-            <ul>
-              <li>01. Henry, Come On</li>
-              <li>02. Bluebird</li>
-              <li>03. White Feather Hawk Tail Deer Hunter</li>
-            </ul>
-
-            <div className="album-mini">
-              <img src="/images/levre.jpeg" alt="detail" />
+            <div className="album-cover">
+              <img src="/images/lanadis.jpeg" alt="Album Cover" />
             </div>
-              <div className="album-mini album-car">
-            <img src="/images/car.jpeg" alt="car detail" />
+
+            <div className="album-about">
+              <h2>About the Album</h2>
+              <p>
+                STOVE is the 10th studio album by Lana Del Rey. A cinematic americana  story about memory, home, nostalgia and love.The Project blends intimate
+                 storytelling with dreamy soundscapes and emotional song writing
+              </p>
+            </div>
+
           </div>
+
+          {/* RIGHT */}
+          <div className="album-right">
+
+            <div className="album-buttons">
+              <button>Stream</button>
+              <button>Download</button>
+            </div>
+
+            <div className="album-tracklist">
+              <h3>Tracklist</h3>
+              <ol>
+                <li> All about Ethel</li>
+                <li> Quiet in the South</li>
+                <li> Past Present Future</li>
+                <li> Stars Fell on Alabama</li>
+                <li> White Feather Hawk Tail Deer Hunter</li>
+                <li> Bluebird</li>
+                <li>more ..</li>
+              </ol>
+            </div>
+
+            
+
           </div>
 
         </div>
